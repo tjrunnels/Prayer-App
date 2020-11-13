bcaf486707378f36cbe549971ddab44143385664
TomR <tjrunnels@liberty.edu>
2020-11-13 10:55:07 -0500

    Initial Commit

---
+++ Prayer App/Prayer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prayer_AppApp.swift
+//  Prayer App
+//
+//  Created by tomrunnels on 11/13/20.
+//
+
+import SwiftUI
+
+@main
+struct Prayer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
