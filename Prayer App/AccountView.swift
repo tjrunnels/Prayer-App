49f28481f3f6804f647803e42a86f1ba6448ccb6
Tom Runnels <tjrunnels@liberty.edu>
2021-03-19 16:20:12 -0400

    removed reference on some files

--- Prayer App/AccountView.swift
+++ Prayer App/AccountView.swift
@@ -1,49 +1,49 @@
+////
+////  AccountView.swift
+////  Prayer App
+////
+////  Created by tomrunnels on 11/28/20.
+////
 //
-//  AccountView.swift
-//  Prayer App
+//import SwiftUI
+//import Amplify
 //
-//  Created by tomrunnels on 11/28/20.
+//struct AccountView: View {
+//    @EnvironmentObject var sessionManager : AuthSessionManager
+//    let user: AuthUser
+//    
+//    var body: some View {
+//        VStack{
+//            Text("Hello, \(user.username) !").font(.largeTitle).padding(.all, 40)
+//            Text("ID: \(user.userId)").font(.body).foregroundColor(.gray)
+//            Spacer()
+//            
+//            Button(action: {
+//                sessionManager.signOut()
+//            }
+//            ){
+//                Text("Sign Out")
+//                .padding()
+//                .foregroundColor(.white)
+//                .background(Color.blue)
+//                .cornerRadius(30)
+//                .scaleEffect(1.0)
+//            }
+//            .padding(.bottom,50)
+//        }
+//        
+//    }
+//    
+//    
+//}
 //
-
-import SwiftUI
-import Amplify
-
-struct AccountView: View {
-    @EnvironmentObject var sessionManager : AuthSessionManager
-    let user: AuthUser
-    
-    var body: some View {
-        VStack{
-            Text("Hello, \(user.username) !").font(.largeTitle).padding(.all, 40)
-            Text("ID: \(user.userId)").font(.body).foregroundColor(.gray)
-            Spacer()
-            
-            Button(action: {
-                sessionManager.signOut()
-            }
-            ){
-                Text("Sign Out")
-                .padding()
-                .foregroundColor(.white)
-                .background(Color.blue)
-                .cornerRadius(30)
-                .scaleEffect(1.0)
-            }
-            .padding(.bottom,50)
-        }
-        
-    }
-    
-    
-}
-
-struct AccountView_Previews: PreviewProvider {
-    private struct leDummyUser: AuthUser {
-       let userId: String = "1"
-       let username: String = "dummyUser"
-   }
-    
-    static var previews: some View {
-        AccountView(user: leDummyUser())
-    }
-}
+//struct AccountView_Previews: PreviewProvider {
+//    private struct leDummyUser: AuthUser {
+//       let userId: String = "1"
+//       let username: String = "dummyUser"
+//   }
+//    
+//    static var previews: some View {
+//        AccountView(user: leDummyUser())
+//    }
+//}
--- Prayer App/ContentView.swift
+++ Prayer App/ContentView.swift
@@ -1,78 +1,78 @@
+////
+////  ContentView.swift
+////  Prayer App
+////
+////  Created by tomrunnels on 11/13/20.
+////  Copied from https://aws.amazon.com/getting-started/hands-on/build-ios-app-amplify/module-one/
+////
+////
 //
-//  ContentView.swift
-//  Prayer App
-//
-//  Created by tomrunnels on 11/13/20.
-//  Copied from https://aws.amazon.com/getting-started/hands-on/build-ios-app-amplify/module-one/
+//import SwiftUI
+//import Amplify
 //
 //
-
-import SwiftUI
-import Amplify
-
-
-
-// this is the main view of our app,
-// it is made of a Table with one line per Prayer
-struct ContentView: View {
-    
-    @EnvironmentObject var sessionManager : AuthSessionManager
-    
-    // add at the begining of ContentView class
-    @State var showCreatePrayer = false
-
-   
-    let user: AuthUser
-    
-    init(user: AuthUser) {
-      //  AWS_Backend.shared.updateSessionData(withSignInStatus: true)
-        self.user = user
-    }
-
-    
-//    @ObservedObject private var sessionData: SessionData = .shared
-
-    // MARK: - begin of main body
-    var body: some View {
-        
-        Text("...you're not supposed to be here! (this is ContentView)")
-    
-        
-    }
-}
-
-
-
-//MARK: - Preview
-// this is use to preview the UI in Xcode
-struct ContentView_Previews: PreviewProvider {
-    
-    private struct leDummyUser: AuthUser {
-       let userId: String = "1"
-       let username: String = "dummyUser"
-   }
-    
-    static var previews: some View {
 //
-//        let _ = prepareTestData()
-//        return ContentView(user: leDummyUser())
-        Text("not built -tom")
-    }
-}
+//// this is the main view of our app,
+//// it is made of a Table with one line per Prayer
+//struct ContentView: View {
 //
-//// this is a test data set to preview the UI in Xcode
-//func prepareTestData() -> SessionData {
-////    let sessionData = SessionData.shared
-//    let desc = "this is a very long description that should fit on multiiple lines.\nit even has a line break\nor two."
+//    @EnvironmentObject var sessionManager : AuthSessionManager
 //
-//    let n1 = Prayer(id: "01", name: "Hello world", description: desc, image: "mic")
-//    let n2 = Prayer(id: "02", name: "A new Prayer", description: desc, image: "phone")
+//    // add at the begining of ContentView class
+//    @State var showCreatePrayer = false
 //
-//    n1.image = Image(systemName: n1.imageName!)
-//    n2.image = Image(systemName: n2.imageName!)
 //
-//    sessionData.prayers = [ n1, n2 ]
+//    let user: AuthUser
 //
-//    return sessionData
+//    init(user: AuthUser) {
+//      //  AWS_Backend.shared.updateSessionData(withSignInStatus: true)
+//        self.user = user
+//    }
+//
+//
+////    @ObservedObject private var sessionData: SessionData = .shared
+//
+//    // MARK: - begin of main body
+//    var body: some View {
+//
+//        Text("...you're not supposed to be here! (this is ContentView)")
+//
+//
+//    }
 //}
 //
+//
+//
+////MARK: - Preview
+//// this is use to preview the UI in Xcode
+//struct ContentView_Previews: PreviewProvider {
+//
+//    private struct leDummyUser: AuthUser {
+//       let userId: String = "1"
+//       let username: String = "dummyUser"
+//   }
+//
+//    static var previews: some View {
+////
+////        let _ = prepareTestData()
+////        return ContentView(user: leDummyUser())
+//        Text("not built -tom")
+//    }
+//}
+////
+////// this is a test data set to preview the UI in Xcode
+////func prepareTestData() -> SessionData {
+//////    let sessionData = SessionData.shared
+////    let desc = "this is a very long description that should fit on multiiple lines.\nit even has a line break\nor two."
+////
+////    let n1 = Prayer(id: "01", name: "Hello world", description: desc, image: "mic")
+////    let n2 = Prayer(id: "02", name: "A new Prayer", description: desc, image: "phone")
+////
+////    n1.image = Image(systemName: n1.imageName!)
+////    n2.image = Image(systemName: n2.imageName!)
+////
+////    sessionData.prayers = [ n1, n2 ]
+////
+////    return sessionData
+////}
+////
